64a7d5ab58e52f18f009c3de858219bd5b94627d
Si Te Feng <stfeng@uwaterloo.ca>
2014-04-29 22:02:34 -0400

    Prepare for new rounds of developments

---
+++ JumpPad/iPadMainSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  JumpPadMainSearchViewController.h
+//  JumpPad
+//
+//  Created by Si Te Feng on 2014-04-29.
+//  Copyright (c) 2014 Si Te Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iPadMainSearchViewController : UIViewController
+
+@end
